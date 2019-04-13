9b732f0d2ce753e6e45451a52e9ae7c61163faa6
Sayantan Pal <sayantan.world98@gmail.com>
2019-04-13 23:38:53 +0530

    Create q1.c

---
+++ Home_Assign/Day_7/q1.c
@@ -0,0 +1,60 @@
+/*
+   AUTHOR : Sayantan Pal
+   DATE : 07.04.2019
+
+   QUESTION :
+   Write a program to find the non-repeated element in an array. The array contains n
+   number of values where all the elements (except one) appear exactly twice
+
+   Example:
+   Input:   array[] = {1, 1, 3, 3, 4, 4, 5}
+   Output:  5
+
+ */
+# include <stdio.h>
+void search(int *arr, int low, int high)
+{
+        if (low > high)
+                return;
+
+        if (low==high)
+        {
+                printf("\nThe required element is %d \n", arr[low]);
+                return;
+        }
+
+        // Find the middle point
+        int mid = (low + high) / 2;
+
+        if (mid%2 == 0)
+        {
+                if (arr[mid] == arr[mid+1])
+                        search(arr, mid+2, high);
+                else
+                        search(arr, low, mid);
+        }
+        else
+        {
+                if (arr[mid] == arr[mid-1])
+                        search(arr, mid+1, high);
+                else
+                        search(arr, low, mid-1);
+        }
+}
+
+
+int main()
+{
+        int array[100], c, n;
+
+        printf("\nEnter number of elements in array: ");
+        scanf("%d", &n);
+
+        printf("\nEnter elements: ");
+
+        for (c = 0; c < n; c++)
+                scanf("%d", &array[c]);
+
+        search(array, 0, n-1);
+        return 0;
+}
